0d5ffec5a0564c77576994c4b1f48fceddafac00
Lucia <Luciajaureguiheisterborg@gmail.com>
2025-10-17 18:43:27 -0300

    DFS terminado

--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -17,13 +17,32 @@ class DepthFirstSearch:
         """
         # Initialize root node
         root = Node("", state=grid.initial, cost=0, parent=None, action=None)
+        if grid.objective_test(root.state):
+            return Solution(root)
+        
+        frontera= StackFrontier()
+        frontera.add(root)
+        
 
         # Initialize explored with the initial state
-        explored = {}
-        explored[root.state] = True
+        expandidos = {}
+        while True:
+            if frontera.is_empty():
+                return NoSolution(expandidos)
+            nodo= frontera.remove()
+            if nodo.state in expandidos:
+                continue 
+            expandidos[nodo.state] = True
+          
+            for accion in grid.actions(nodo.state):
+                nuevo_estado=grid.result(nodo.state,accion)
+                if nuevo_estado not in expandidos:
+                    nodo_hijo=Node("",nuevo_estado,nodo.cost + grid.individual_cost(nodo.state,accion), nodo,accion)
+                    if grid.objective_test(nuevo_estado):
+                        return Solution(nodo_hijo,expandidos)
+                    frontera.add(nodo_hijo)
+
 
         # Initialize frontier with the root node
         # TODO Complete the rest!!
         # ...
-
-        return NoSolution(explored)
