b6a4c87a85786a2a3fabe55a308c079c8e5eb02c
Lucia <Luciajaureguiheisterborg@gmail.com>
2025-10-17 18:44:37 -0300

    UCS terminado

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -17,13 +17,26 @@ class UniformCostSearch:
         """
         # Initialize root node
         root = Node("", state=grid.initial, cost=0, parent=None, action=None)
+        
+        frontera= PriorityQueueFrontier()
+        
+        frontera.add(root,root.cost)
+        alcanzados = {}
+        alcanzados[root.state]=root.cost
 
-        # Initialize reached with the initial state
-        reached = {}
-        reached[root.state] = root.cost
-
-        # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
-
-        return NoSolution(reached)
+        while True:
+        
+            if frontera.is_empty():
+                return NoSolution(alcanzados)
+            nodo= frontera.pop()
+          
+            if grid.objective_test(nodo.state):
+             return Solution(nodo, alcanzados)
+        
+            for accion in grid.actions(nodo.state):
+                nuevo_estado=grid.result(nodo.state,accion)
+                costo=nodo.cost + grid.individual_cost(nodo.state,accion)
+                if nuevo_estado not in alcanzados or costo < alcanzados[nuevo_estado]:
+                    nodo_hijo = Node("",nuevo_estado, cost=costo,parent=nodo, action=accion)
+                    alcanzados[nuevo_estado]=costo
+                    frontera.add(nodo_hijo,costo)
